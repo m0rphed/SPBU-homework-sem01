6d6c62cf8c35168a38b42bc306840f61669917fb
teaetik <victorKhovTko@yandex.ru>
2018-09-20 13:19:38 +0300

    hot fix

--- problemset-01/02_div.cpp
+++ problemset-01/02_div.cpp
@@ -4,7 +4,8 @@
 int main()
 {
 	long long a = 0, b = 0;
-	int sign = 1, counter = 0;
+	int sign = 1;
+	int counter = 0;
 
 	printf("Enter the dividend:\n  a = ");
 	scanf("%lld", &a);
@@ -12,8 +13,16 @@ int main()
 	printf("Enter the divisor:\n  b = ");
 	scanf("%lld", &b);
 
-	if ((a > 0 && b < 0) || (a < 0 && b > 0))
+	if (b == 0)
+	{
+		printf("Error: devision by 0\n");
+		return 0;
+	}
+
+	if (a < 0 ^ b < 0)
+	{
 		sign *= -1;
+	}
 
 	a = abs(a);
 	b = abs(b);
--- problemset-01/04_tickets.cpp
+++ problemset-01/04_tickets.cpp
@@ -3,7 +3,8 @@
 int main()
 {
 
-	unsigned int counter = 0;
+	int array[28]{};
+	int sum = 0;
 
 	for (int i = 0; i < 10; i++)
 	{
@@ -11,21 +12,15 @@ int main()
 		{
 			for (int k = 0; k < 10; k++)
 			{
-				for (int l = 0; l < 10; l++)
-				{
-					for (int m = 0; m < 10; m++)
-					{
-						for (int n = 0; n < 10; n++)
-						{
-							if (i + j + k == l + m + n)
-								counter++;
-						}
-					}
-				}
+				++array[i + j + k];
 			}
 		}
 	}
+	for (auto elem : array)
+	{
+		sum += elem * elem;
+	}
 
-	printf("Number of lucky tickets: %u", counter);
+	printf("Number of lucky tickets: %d", sum);
 	return 0;
 }
