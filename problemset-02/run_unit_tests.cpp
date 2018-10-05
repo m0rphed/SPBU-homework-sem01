ee37cfac89fd8f075356c630c2c444400d677e62
teaetik <victorKhovTko@yandex.ru>
2018-10-05 12:48:47 +0300

    Added task-2.1 with related tests.

--- problemset-02/run_unit_tests.cpp
+++ problemset-02/run_unit_tests.cpp
@@ -1,89 +1,193 @@
-#include <stdio.h>
 #include <gtest/gtest.h> // Google Test Framework
+#include <gmock/gmock.h> /* Google Mock
+							Google Mock works seamlessly with Google Test.
+							It comes with a copy of Google Test bundled. */
+
+#include <stdio.h>       // puts()
+#include <stdlib.h>      // qsort(), srand()
+
+// using arrayHelpers.h for more efficient array initialization etc.
+#include "arrayHelpers.h"
 
 // include all solutions from problemset-02
-#include "01_fibonacci.cpp"
-#include "02_power.cpp"
-#include "03_counting_bubbles.cpp"
-#include "04_sort_rand_array.cpp"
+#include "task_01_fibonacci.h"
+#include "task_02_power.h"
+#include "task_03_counting_bubbles.h"
+#include "task_04_sort_rand_array.h"
 
 
-TEST(FibonacciTest, BasicTests)
+::testing::AssertionResult strangeSortTestFunc(int *testArray, const int size)
 {
-	const int fib_1 = 1;
-	ASSERT_EQ(fib_1, recursiveFib(0, 1, 1));
-	ASSERT_EQ(fib_1, iterativeFib(1));
-	ASSERT_EQ(fib_1, analyticFib(1));
+	bool success = true;
 	
-	const int fib_2 = 1;
-	ASSERT_EQ(fib_2, recursiveFib(0, 1, 2));
-	ASSERT_EQ(fib_2, iterativeFib(2));
-	ASSERT_EQ(fib_2, analyticFib(2));
+	for (int i = 2; i < size; i++)
+	{
+		if (testArray[i - 1] >= testArray[0])
+		{
+			if (testArray[i] < testArray[0])
+			{
+				success = false;
+			}
+		}
+	}
 	
-	const int fib_10 = 55;
-	ASSERT_EQ(fib_10, recursiveFib(0, 1, 10));
-	ASSERT_EQ(fib_10, iterativeFib(10));
-	ASSERT_EQ(fib_10, analyticFib(10));
+	if (success)
+	{
+		return ::testing::AssertionSuccess();
+	}
+	else
+	{
+		return ::testing::AssertionFailure();
+	}
+}
+
+
+TEST(fibonacciTest, basicTests)
+{
+	// The first value of the pair — the number of the element of the Fibonacci sequence.
+	// The second value of the pair — expected value.
+	const int testValues[][2] = {{1,  1},
+	                             {2,  1},
+	                             {10, 55},
+	                             {37, 24157817},
+	                             {45, 1134903170}};
 	
-	const int fib_37 = 24157817;
-	ASSERT_EQ(fib_37, recursiveFib(0, 1, 37));
-	ASSERT_EQ(fib_37, iterativeFib(37));
-	ASSERT_EQ(fib_37, analyticFib(37));
-	
-	const int fib_45 = 1134903170;
-	ASSERT_EQ(fib_45, recursiveFib(0, 1, 45));
-	ASSERT_EQ(fib_45, iterativeFib(45));
-	ASSERT_EQ(fib_45, analyticFib(45));
+	for (const auto valuePair : testValues)
+	{
+		// Test different implementations
+		ASSERT_EQ(valuePair[1], recursiveFib(valuePair[0]));
+		ASSERT_EQ(valuePair[1], iterativeFib(valuePair[0]));
+		ASSERT_EQ(valuePair[1], analyticFib(valuePair[0]));
+		ASSERT_EQ(valuePair[1], matrixFib(valuePair[0]));
+	}
 	
 	puts("\n\tAll tests for task 'fibonacci' -- passed!\n");
 }
 
-TEST(RaisedToThePowerTest, BinaryPow)
+
+TEST(raisedToThePowerTest, basicTests)
 {
-	const int evenPower = 10;
-	const int oddPower = 9;
+	int powerValues[] = {0, 1, 9, 10};
+	int testValues[] = {0, -1, 1, -2, 2, -3, 3};
 	
-	const int zero = 0;
-	ASSERT_EQ(1, binaryPow(zero, 0));
-	ASSERT_EQ(zero, binaryPow(zero, 1));
-	ASSERT_EQ(zero, binaryPow(zero, evenPower));
-	ASSERT_EQ(zero, binaryPow(zero, oddPower));
-	
-	ASSERT_EQ(1, binaryPow(2, 0));
-	ASSERT_EQ(2, binaryPow(2, 1));
-	ASSERT_EQ(1024, binaryPow(2, evenPower));
-	ASSERT_EQ(512, binaryPow(2, oddPower));
-	
-	ASSERT_EQ(1, binaryPow(-2, 0));
-	ASSERT_EQ(-2, binaryPow(-2, 1));
-	ASSERT_EQ(1024, binaryPow(-2, evenPower));
-	ASSERT_EQ(-512, binaryPow(-2, oddPower));
-	
-	puts("\n\tAll tests for function 'binaryPow' -- passed!\n");
+	// Let's do some tests!
+	for (const auto testCase : testValues)
+	{
+		for (const auto power: powerValues)
+		{
+			ASSERT_EQ(pow(testCase, power), binaryPow(testCase, power));
+			ASSERT_EQ(pow(testCase, power), simplePow(testCase, power));
+		}
+	}
+	puts("\n\tAll tests for task 'power' -- passed!\n");
 }
 
-TEST(RaisedToThePowerTest, SimplePow)
+
+TEST(bubbleSortTest, randomArrayTests)
 {
-	const int evenPower = 10;
-	const int oddPower = 9;
+	// use current time as seed for random generator
+	srand(time(nullptr));
 	
-	const int zero = 0;
-	ASSERT_EQ(1, simplePow(zero, 0));
-	ASSERT_EQ(zero, simplePow(zero, 1));
-	ASSERT_EQ(zero, simplePow(zero, evenPower));
-	ASSERT_EQ(zero, simplePow(zero, oddPower));
+	// set the size of arrays
+	const int size = 15;
 	
-	ASSERT_EQ(1, simplePow(2, 0));
-	ASSERT_EQ(2, simplePow(2, 1));
-	ASSERT_EQ(1024, simplePow(2, evenPower));
-	ASSERT_EQ(512, simplePow(2, oddPower));
+	// set the number of tests
+	const int numOfTests = 5;
 	
-	ASSERT_EQ(1, simplePow(-2, 0));
-	ASSERT_EQ(-2, simplePow(-2, 1));
-	ASSERT_EQ(1024, simplePow(-2, evenPower));
-	ASSERT_EQ(-512, simplePow(-2, oddPower));
+	int testArrays[numOfTests][size] = {};
+	int expectedArrays[numOfTests][size] = {};
 	
-	puts("\n\tAll tests for function 'simplePow' -- passed!\n");
+	for (auto testArr : testArrays)
+	{
+		// fill every element of testArrays with random values
+		fillArray(testArr, size);
+	}
+	
+	for (int i = 0; i < numOfTests; ++i)
+	{
+		copyArray(expectedArrays[i], testArrays[i], size);
+		
+		// and then sort every element using qsort()
+		qsort(expectedArrays[i], size, sizeof(int), compareIncreasing);
+	}
+	
+	for (int i = 0; i < numOfTests; ++i)
+	{
+		bubbleSort(testArrays[i], size);
+		ASSERT_THAT(testArrays[i], testing::ElementsAreArray(expectedArrays[i]));
+	}
+	
+	puts("\n\tAll tests for function 'bubbleSort' -- passed!\n");
+}
+
+
+TEST(countingSortTest, randomArrTest)
+{
+	// use current time as seed for random generator
+	srand(time(nullptr));
+	
+	// set the size of arrays
+	const int size = 15;
+	
+	// set the number of tests
+	const int numOfTests = 5;
+	
+	// set the range for values of arrays
+	const int minVal = 0;
+	const int maxVal = 1000;
+	
+	int testArrays[numOfTests][size] = {};
+	int expectedArrays[numOfTests][size] = {};
+	
+	for (auto testArr : testArrays)
+	{
+		// fill every element of testArrays with random values
+		fillArray(testArr, size, minVal, maxVal);
+	}
+	
+	for (int i = 0; i < numOfTests; ++i)
+	{
+		copyArray(expectedArrays[i], testArrays[i], size);
+		
+		// and then sort every element using qsort()
+		qsort(expectedArrays[i], size, sizeof(int), compareIncreasing);
+	}
+	
+	for (int i = 0; i < numOfTests; ++i)
+	{
+		countingSort(testArrays[i], size);
+		ASSERT_THAT(testArrays[i], testing::ElementsAreArray(expectedArrays[i]));
+	}
+	
+	puts("\n\tAll tests for function 'countingSort' -- passed!\n");
+}
+
+
+TEST(strangeSort, randomArraysTest)
+{
+	// use current time as seed for random generator
+	srand(time(nullptr));
+	
+	// set the size of arrays
+	const int size = 15;
+	
+	// set the number of tests
+	const int numOfTests = 5;
+	
+	int testArrays[numOfTests][size] = {};
+	
+	for (auto array : testArrays)
+	{
+		fillArray(array, size);
+	}
+	
+	for (auto array : testArrays)
+	{
+		strangeSort(array, size);
+		EXPECT_TRUE(strangeSortTestFunc(array, size));
+	}
+	
+	puts("\n\tAll tests for task 'strangeSort' -- passed!\n");
 }
 
 
@@ -95,7 +199,7 @@ int main(int argc, char **argv)
 	// Force print tests times
 	testing::GTEST_FLAG(print_time) = true;
 	
-	puts("\nRunning main() from run_unit_test.cc\n");
+	puts("\nRunning main() from run_unit_test.cpp\n");
 	
 	return RUN_ALL_TESTS();
 }
