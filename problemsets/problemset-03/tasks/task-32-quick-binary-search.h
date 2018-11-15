43b81a23dea71db559baa0c22c9bccefe6f72f20
Victor Khovanov <victorKhovTko@yandex.ru>
2018-11-16 00:54:33 +0300

    Beautify task-32

--- problemsets/problemset-03/tasks/task-32-quick-binary-search.h
+++ problemsets/problemset-03/tasks/task-32-quick-binary-search.h
@@ -1,8 +1,11 @@
-//
-// Created by vic on 11/12/18.
-//
+#pragma once
 
-#ifndef SPBU_HOMEWORK_TASK_32_QUICK_BINARY_SEARCH_H
-#define SPBU_HOMEWORK_TASK_32_QUICK_BINARY_SEARCH_H
 
-#endif //SPBU_HOMEWORK_TASK_32_QUICK_BINARY_SEARCH_H
+int binarySearch(int *array, int length, int item);
+
+
+// | qBinarySearch function:
+// | do quickSort() for an array (which size = n):                      => O(n*log(n))
+// | do binarySearch() for arrayOfNums (which contains k numbers):      => O(k*log(n))
+// | expected time complexity:                                          => O(n*log(n) + k*log(n))
+void qBinarySearch(int *array, int n, int *arrayOfNums, int k, bool *foundMatches);
