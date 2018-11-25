d4b30deafe932dd47e90da07f4dff183b5eb42f3
Victor Khovanov <victorKhovTko@yandex.ru>
2018-11-25 22:03:38 +0300

    Update tests

--- problemsets/problemset-06/utests/utest-task-61.cpp
+++ problemsets/problemset-06/utests/utest-task-61.cpp
@@ -1,8 +1,33 @@
+#include "../tasks/ReversePolishNotation/task-61-postfix-notation.h"
 #include <gtest/gtest.h> // Google Test Framework
-#include <gmock/gmock.h>
+#include <vector>
+#include <string>
+
+using namespace std;
 
 
-TEST(problemset06, task61)
+vector<string> handleUserInput(const string &input)
 {
-	ASSERT_TRUE(false);
+	cout << "\nInput mathematical operation in Reverse Polish Notation:\n" << input << endl;
+	
+	// create vector of substrings split by space character
+	istringstream stream(input);
+	vector<string> items{istream_iterator<string>{stream}, istream_iterator<string>{}};
+	
+	return items;
+}
+
+
+TEST(problemset06, ReversePolishTest)
+{
+	// Create an instance of calculationStack class.
+	auto *calculator = new calculationStack(); // calculator points to nullptr
+	
+	const string testString = "9 6 - 1 2 + *\n";
+
+	// Simulate user input and do calculation
+	calculator->performCalculations(handleUserInput(testString));
+	
+	cout << "Result:\n" << calculator->getResult() << endl;
+	ASSERT_EQ(9, calculator->getResult());
 }
--- problemsets/problemset-06/utests/utest-task-62.cpp
+++ problemsets/problemset-06/utests/utest-task-62.cpp
@@ -1,8 +1,7 @@
 #include <gtest/gtest.h> // Google Test Framework
-#include <gmock/gmock.h>
 
 
 TEST(problemset06, task62)
 {
-	ASSERT_TRUE(false);
+	FAIL();
 }
