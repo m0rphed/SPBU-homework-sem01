0239fdce4be3047bda291b599f671e6ab6031495
Victor Khovanov <victorKhovTko@yandex.ru>
2018-12-20 15:58:22 +0300

    Added solution

--- problemsets/TestWorks/testwork-2-4/testwork-2-4-matrix-sort.cpp
+++ problemsets/TestWorks/testwork-2-4/testwork-2-4-matrix-sort.cpp
@@ -5,17 +5,6 @@
 typedef int ElementType;
 using namespace std;
 
-void printMatrix(ElementType &&myMatrix, const int &numberOfRows, const int &numberOfColumns)
-{
-	for (int i = 0; i < numberOfRows; ++i)
-	{
-		for (int j = 0; j < numberOfColumns; ++j)
-		{
-			cout <<
-		}
-	}
-}
-
 vector<ElementType> getFirstRow(vector<vector<ElementType>> &myMatrix)
 {
 	const unsigned long numberOfColumns = myMatrix[0].size();
@@ -27,13 +16,19 @@ vector<ElementType> getFirstRow(vector<vector<ElementType>> &myMatrix)
 	}
 }
 
-void swapColumns(vector<vector<ElementType>> &myMatrix, const int &i, const int &j)
-{
 
+void swapColumns(vector<vector<ElementType>> &myMatrix, const int &firstColumn, const int &secondColumn)
+{
+	const unsigned long numberOfRows = myMatrix.size();
+	const unsigned long numberOfColumns = myMatrix[0].size();
+	
+	for (auto &row : myMatrix)
+	{
+		swap(row[firstColumn], row[secondColumn]);
+	}
 }
 
 
-// Sort matrix columns by first element
 void sortMatrixColumns(vector<vector<ElementType>> &myMatrix)
 {
 	const unsigned long numberOfRows = myMatrix.size();
@@ -43,30 +38,25 @@ void sortMatrixColumns(vector<vector<ElementType>> &myMatrix)
 	
 	for (int j = 0; j < numberOfColumns - 1; ++j)
 	{
-		if (&order[j] > &order[j+1])
+		for (int t = 0; t < numberOfColumns - j - 1; ++t)
 		{
-		
+			if (&order[t] > &order[t + 1])
+			{
+				// меняем элементы местами
+				swapColumns(myMatrix, t, t + 1);
+			}
 		}
 	}
-	
-	for ()
-	{
-	}
-	for (auto & : myMatrix)
-	{
-	
-	}
-	std::swap(v[2], v[3]);
-	for (int i = 0; i < numberOfRows; i++)
-	{
-		cout << v[i] << " ";
-	}
-	return 0;
-	
 }
 
 
-void createVector(vector<vector<ElementType>> &myMatrix)
+void printMatrix(vector<vector<ElementType>> &myMatrix, const int &numberOfRows, const int &numberOfColumns)
 {
-
+	for (int i = 0; i < numberOfRows; ++i)
+	{
+		for (int j = 0; j < numberOfColumns; ++j)
+		{
+			cout << myMatrix[i][j] << endl;
+		}
+	}
 }
--- problemsets/TestWorks/testwork-2-4/testwork-2-4-matrix-sort.h
+++ problemsets/TestWorks/testwork-2-4/testwork-2-4-matrix-sort.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#include <vector>
+
+typedef int ElementType;
+using namespace std;
+
+vector<ElementType> getFirstRow(vector<vector<ElementType>> &myMatrix);
+
+void swapColumns(vector<vector<ElementType>> &myMatrix, const int &firstColumn, const int &secondColumn);
+
+// Sort matrix columns by first element
+void sortMatrixColumns(vector<vector<ElementType>> &myMatrix);
+
+void printMatrix(vector<vector<ElementType>> &myMatrix, const int &numberOfRows, const int &numberOfColumns);
