23c6915067eab3f1ce60bd403889b37f4be14254
Victor Khovanov <victorKhovTko@yandex.ru>
2018-12-21 17:10:18 +0300

    Trying to fix conflicts in pathes

--- problemsets/TestWorks/BossFight/dataStructures/PriorityQueue.cpp
+++ problemsets/TestWorks/BossFight/dataStructures/PriorityQueue.cpp
@@ -1,5 +1,6 @@
 #include "PriorityQueue.h"
 
+<<<<<<< HEAD
 // Constructor
 PriorityQueue::PriorityQueue()
 {
@@ -142,3 +143,126 @@ node *PriorityQueue::findHighestNode()
 	return highest;
 }
 
+=======
+//
+//bool PriorityQueue::empty()
+//{
+//	return (head == nullptr);
+//}
+//
+//
+//int PriorityQueue::size()
+//{
+//	return numberOfNodes;
+//}
+//
+//
+//int PriorityQueue::front()
+//{
+//	if (head)
+//	{
+//		return head->value;
+//	}
+//	else
+//	{
+//		return -1;
+//	}
+//}
+//
+//
+//int PriorityQueue::back()
+//{
+//	for (tail = head; tail->next != nullptr; tail = tail->next);
+//	return tail->value;
+//}
+//
+//
+//void PriorityQueue::enqueue(const int &newValue)
+//{
+//	node *temp;
+//	node *current;
+//
+//	temp = new node;
+//	temp->value = newValue;
+//
+//	// Inserting in the beginning of the queue
+//	if (head == nullptr || newValue < head->value)
+//	{
+//		temp->next = head;
+//		head = temp;
+//	}
+//		// Inserting middle/end of queue
+//	else
+//	{
+//		current = head;
+//
+//		// Go through the list until the next node value
+//		// is LARGER than the current value,
+//		// but stop once we hit the end of the list
+//		while (current->next != nullptr && current->next->value < newValue)
+//		{
+//			current = current->next;
+//		}
+//		temp->next = current->next;
+//		current->next = temp;
+//	}
+//
+//	++numberOfNodes;
+//}
+//
+//
+//void PriorityQueue::dequeue()
+//{
+//	node *temp = nullptr;
+//
+//	if (head)
+//	{
+//		temp = head;
+//		head = head->next;
+//
+//		delete temp;
+//		--numberOfNodes;
+//	}
+//}
+//
+//
+//// Constructor
+//PriorityQueue::PriorityQueue()
+//{
+//	head = tail = nullptr;
+//	numberOfNodes = 0;
+//}
+//
+//
+//void queueTest()
+//{
+//	PriorityQueue myQueue = PriorityQueue();
+//
+//	cout << "The queue should be empty: " << myQueue.empty() << endl;
+//	cout << "The size of the queue should be 0: " << myQueue.size() << endl;
+//	myQueue.enqueue(3);
+//	cout << "myQueue.front() = " << myQueue.front() << " myQueue.back() = " << myQueue.back() << endl;
+//	myQueue.enqueue(1);
+//	cout << "myQueue.front() = " << myQueue.front() << " myQueue.back() = " << myQueue.back() << endl;
+//	myQueue.enqueue(2);
+//	myQueue.dequeue();
+//	cout << "The size of the queue should be 2: " << myQueue.size() << endl;
+//
+//	cout << "myQueue.front() = " << myQueue.front() << " myQueue.back() = " << myQueue.back() << endl;
+//	myQueue.enqueue(4);
+//	cout << "myQueue.front() = " << myQueue.front() << " myQueue.back() = " << myQueue.back() << endl;
+//	myQueue.enqueue(5);
+//	cout << "myQueue.front() = " << myQueue.front() << " myQueue.back() = " << myQueue.back() << endl;
+//	myQueue.dequeue();
+//
+//	cout << "The queue should NOT be empty: " << myQueue.empty() << endl;
+//	cout << "The size of the queue should be 3: " << myQueue.size() << endl;
+//
+//	// Print out the queue
+//	while (!myQueue.empty())
+//	{
+//		cout << myQueue.front() << " " << endl;
+//		myQueue.dequeue();
+//	}
+//}
+>>>>>>> parent of 917e341... Updated
--- problemsets/TestWorks/BossFight/tasks/bossFight-task02.cpp
+++ problemsets/TestWorks/BossFight/tasks/bossFight-task02.cpp
@@ -7,43 +7,8 @@
 // принимающую на вход значение и численный приоритет, и функцию dequeue(),
 // возвращающую значение с наивысшим приоритетом и удаляющую его из очереди.
 // Если очередь пуста, dequeue() должен возвращать -1.
-
 int main()
 {
-	auto *myQueue = new PriorityQueue;
-	
-	int command = 0;
-	int value = 0;
-	int key = 0;
-	
-	do
-	{
-		cout << "Choose operation: " << endl;
-		cout << "\t0) Exit" << endl;
-		cout << "\t1) Enqueue" << endl;
-		cout << "\t2) Dequeue" << endl;
-		cout << "\t3) Print" << endl;
-		
-		cin >> command;
-		if (command == 1)
-		{
-			cin >> value >> key;
-			myQueue->enqueue(value, key);
-		}
-		else if (command == 2)
-		{
-			myQueue->dequeue();
-		}
-		else if (command == 3)
-		{
-			myQueue->print();
-		}
-		else
-		{
-			cout << "Wrong command" << endl;
-			break;
-		}
-	} while (command != 0);
-	
+	queueTest();
 	return 0;
 }
