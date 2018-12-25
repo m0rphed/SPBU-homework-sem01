631deef088470c5e019c57b9e8b1bb6b88aef3b9
Victor Khovanov <victorKhovTko@yandex.ru>
2018-12-25 20:02:35 +0300

    Updated at last

--- problemsets/TestWorks/testwork-2-5/unitTest-process-calendar.cpp
+++ problemsets/TestWorks/testwork-2-5/unitTest-process-calendar.cpp
@@ -24,8 +24,8 @@ TEST(testwork_2, requestTest2)
 {
 	auto *myQueue = new Queue();
 	
-	const int n = 10;
-	int arrayOfCommands[n] = {1, -1, 3, 4, 5, 6, -1, 8, -1, 10};
+	const int n = 11;
+	int arrayOfCommands[n] = {1, -1, -1, 3, 4, 5, 6, -1, 8, -1, 10};
 	
 	handleInput(n, arrayOfCommands, myQueue);
 	
