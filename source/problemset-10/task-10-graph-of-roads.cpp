295aaef83a22dc88e114fe07e328b94f2eac8440
Victor Khovanov <victorKhovTko@yandex.ru>
2018-12-28 11:43:15 +0300

    Added problem description + DataInput.txt

--- source/problemset-10/task-10-graph-of-roads.cpp
+++ source/problemset-10/task-10-graph-of-roads.cpp
@@ -53,6 +53,36 @@ void buildWorld(DataInput *dInput, DataOutput *dOutput)
 }
 
 
+// Sample of input:
+//
+//
+// 9       - number of cities
+// 11      - number or roads
+// 1 2 2
+// 1 3 1
+// 1 9 15
+// 3 4 18
+// 3 7 8
+// 4 5 4
+// 5 6 43
+// 6 7 21
+// 7 8 10
+// 7 9 30
+// 8 9 9
+// 3       - number of countries (each country has 1 capital)
+// 2 1 4   - each capital (1 per country)
+//
+//
+// Plan:
+//
+// - create graph
+// - create list of countries
+// - mark capitals as allocated
+// - every time
+// - find adjacent nodes for all existing (not allocated)
+// - select and allocate the node
+
+
 int main(int argc, char **argv)
 {
 	::testing::InitGoogleTest(&argc, argv);
