920a0b615380b38db85c16c090007c33f6841cd0
Victor Khovanov <victorKhovTko@yandex.ru>
2018-12-29 00:43:44 +0300

    Task -> Phone Book finally done :c

--- source/problemset-04/tasks/task-43-phonebook/task-43-phonebook.h
+++ source/problemset-04/tasks/task-43-phonebook/task-43-phonebook.h
@@ -1,4 +1,23 @@
 #pragma once
 
+#include <string>
 
-void phoneBookHandler();
+struct Record
+{
+	std::string name;
+	long phoneNumber = 0;
+};
+
+void processData(const std::string &filename, Record *phoneBook, int &size);
+
+void saveData(const std::string &filename, Record *phoneBook, const int &size);
+
+void addRecord(const std::string &newName, const long &newNumber, Record *phoneBook, int &size);
+
+std::string findName(const long &number, const Record *phoneBook, const int &size);
+
+long findNumber(const std::string &name, const Record *phoneBook, const int &size);
+
+void printAll(const Record *phoneBook, const int &size);
+
+void phoneBookUserInterface();
