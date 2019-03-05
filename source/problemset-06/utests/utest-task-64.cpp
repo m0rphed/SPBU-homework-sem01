8c01acf9997f444c4d7b596228e612cee13541ae
John Doe <victorKhovTko@yandex.ru>
2019-03-05 14:28:00 +0300

    Changed unit test, saved sorted data and it's finally done

--- source/problemset-06/utests/utest-task-64.cpp
+++ source/problemset-06/utests/utest-task-64.cpp
@@ -11,8 +11,15 @@ TEST(task64, mergeSortTest)
     auto *list = processData("data.txt");
     list->printList();
 
+    cout << "\nSort by phone number" << endl;
     mergeSort(list, false);
     list->printList();
+    saveData(list, "data-by-number.txt");
+
+    cout << "\nSort by name" << endl;
+    mergeSort(list);
+    list->printList();
+    saveData(list, "data-by-name.txt");
 
     delete list;
     SUCCEED();
